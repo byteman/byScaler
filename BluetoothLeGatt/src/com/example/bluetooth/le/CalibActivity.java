473c38674d4e62a6d3d1ccdd928a2938c7589d05
wangchen <154554381@qq.com>
2015-04-10 14:54:53 +0800

    美化标定界面

--- BluetoothLeGatt/src/com/example/bluetooth/le/CalibActivity.java
+++ BluetoothLeGatt/src/com/example/bluetooth/le/CalibActivity.java
@@ -1,11 +1,89 @@
 package com.example.bluetooth.le;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.xtremeprog.sdk.ble.BleGattCharacteristic;
+import com.xtremeprog.sdk.ble.BleService;
+import com.xtremeprog.sdk.ble.IBle;
 
 public class CalibActivity extends Activity {
 
-	/* (non-Javadoc)
+	private String mDeviceAddress;
+	private int ad = 0;
+	private IBle mBle;
+	private BleGattCharacteristic mCharacteristic;
+	private final String TAG = "CalibActivity";
+	private TextView m_tvAD;
+	private Button m_btCalibZero, m_btCalibWgt;
+	private EditText m_etZero, m_etWgt;
+
+	private final class ButtonListener implements View.OnClickListener {
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			if (v.getId() == R.id.btCalbZero) {
+				m_etZero.setText(m_tvAD.getText());
+			} else if (v.getId() == R.id.btCalbWgt) {
+				if (m_etWgt.getText().length() <= 0) {
+					Toast.makeText(CalibActivity.this, "请先输入内容",
+							Toast.LENGTH_LONG).show();
+				}
+			}
+
+		}
+	}
+
+	private final BroadcastReceiver mBleReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Bundle extras = intent.getExtras();
+
+			String action = intent.getAction();
+			Log.e(TAG, action);
+			if (BleService.BLE_GATT_DISCONNECTED.equals(action)) {
+
+				finish();
+			} else if (BleService.BLE_CHARACTERISTIC_READ.equals(action)
+					|| BleService.BLE_CHARACTERISTIC_CHANGED.equals(action)) {
+				byte[] val = extras.getByteArray(BleService.EXTRA_VALUE);
+
+				ad = Utils.byte2Int(val);
+
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+
+						m_tvAD.setText(String.valueOf(ad));
+					}
+				});
+
+			} else if (BleService.BLE_CHARACTERISTIC_WRITE.equals(action)) {
+				Toast.makeText(CalibActivity.this, "Write success!",
+						Toast.LENGTH_SHORT).show();
+			}
+
+		}
+	};
+	private ActionBar mActionBar;
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
 	@Override
@@ -13,6 +91,75 @@ public class CalibActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.calib);
+		init();
+
+	}
+
+	private void init() {
+		// TODO Auto-generated method stub
+		mActionBar = getActionBar();
+		// 设置是否显示应用程序的图标
+		mActionBar.setDisplayShowHomeEnabled(true);
+		// 将应用程序图标设置为可点击的按钮
+		mActionBar.setHomeButtonEnabled(true);
+		// 将应用程序图标设置为可点击的按钮,并且在图标上添加向左的箭头
+		// 该句代码起到了决定性作用
+		mActionBar.setDisplayHomeAsUpEnabled(true);
+		m_tvAD = (TextView) findViewById(R.id.tvAD);
+		m_btCalibZero = (Button) findViewById(R.id.btCalbZero);
+		m_btCalibWgt = (Button) findViewById(R.id.btCalbWgt);
+		m_etZero = (EditText) findViewById(R.id.etZero);
+		m_etWgt = (EditText) findViewById(R.id.etWgt);
+		final View.OnClickListener pClickListener = new ButtonListener();
+
+		m_btCalibZero.setOnClickListener(pClickListener);
+		m_btCalibWgt.setOnClickListener(pClickListener);
+
+		mDeviceAddress = getIntent().getStringExtra("address");
+		String service = getIntent().getStringExtra("service");
+		String characteristic = getIntent().getStringExtra("characteristic");
+		BleApplication app = (BleApplication) getApplication();
+		mBle = app.getIBle();
+		if (mBle == null) {
+			Toast.makeText(this, "BLE not find", Toast.LENGTH_LONG).show();
+			return;
+		}
+		mCharacteristic = mBle.getService(mDeviceAddress,
+				UUID.fromString(service)).getCharacteristic(
+				UUID.fromString(characteristic));
+
+		if (mCharacteristic == null) {
+			Toast.makeText(this, "Characteristic not find", Toast.LENGTH_LONG)
+					.show();
+			return;
+		}
+		new Timer().schedule(new TimerTask() {
+			public void run() {
+
+				mBle.requestReadCharacteristic(mDeviceAddress, mCharacteristic);
+			}
+		}, 0, 100);
+
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		registerReceiver(mBleReceiver, BleService.getIntentFilter());
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		unregisterReceiver(mBleReceiver);
 	}
 
 }
--- BluetoothLeGatt/src/com/example/bluetooth/le/WeightActivity.java
+++ BluetoothLeGatt/src/com/example/bluetooth/le/WeightActivity.java
@@ -1,5 +1,13 @@
 package com.example.bluetooth.le;
 
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -18,64 +26,54 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Date;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.UUID;
-
 import com.xtremeprog.sdk.ble.BleGattCharacteristic;
-import com.xtremeprog.sdk.ble.IBle;
 import com.xtremeprog.sdk.ble.BleService;
+import com.xtremeprog.sdk.ble.IBle;
 
 public class WeightActivity extends Activity {
-	class WeightData
-	{
-		
+	private class WeightData {
+
 		public String sid;
 		public String stime;
 		public String skg;
-		public WeightData(int id, String kg)
-		{
+
+		public WeightData(int id, String kg) {
 			sid = String.valueOf(id);
-			//Date date = new Date();
-		//   System.out.println("����ת�ַ�" + HelloTest.DateToStr(date));
-			long time =System.currentTimeMillis();
-			
+			// Date date = new Date();
+			// System.out.println("����ת�ַ�" + HelloTest.DateToStr(date));
+			long time = System.currentTimeMillis();
+
 			stime = getCurrentTime(time);
-			skg = kg+"kg";
+			skg = kg + "kg";
 		}
-		
+
 	};
-	//final int weight = 0;
+
+	// final int weight = 0;
 	private int index = 0;
 	private int weight = 0;
 	private String mDeviceAddress;
+	private String mService;
+	private String mCharacteristics;
 	private IBle mBle;
 	private boolean mNotifyStarted;
 	private BleGattCharacteristic mCharacteristic;
-	private TextView txtWgt ;
+	private TextView txtWgt;
 	private ListView listData;
-	public MyAdapter adapter; 
+	public MyAdapter adapter;
 	protected static final String TAG = "weight";
-    public static String getCurrentTime(long date) 
-    {
-    	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    	String str = format.format(new Date(date));
-    	return str;
-    }
-    /**
-     * 4λ�ֽ�����ת��Ϊ����
-     * @param b
-     * @return
-     */
+
+	public static String getCurrentTime(long date) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String str = format.format(new Date(date));
+		return str;
+	}
 
 	private final BroadcastReceiver mBleReceiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			Bundle extras = intent.getExtras();
-			
+
 			if (!mDeviceAddress.equals(extras.getString(BleService.EXTRA_ADDR))) {
 				return;
 			}
@@ -87,17 +85,17 @@ public class WeightActivity extends Activity {
 			}
 
 			String action = intent.getAction();
-			Log.e(TAG,action);
+			Log.e(TAG, action);
 			if (BleService.BLE_GATT_DISCONNECTED.equals(action)) {
-				Toast.makeText(WeightActivity.this,
-						"Device disconnected...", Toast.LENGTH_SHORT).show();
+				Toast.makeText(WeightActivity.this, "Device disconnected...",
+						Toast.LENGTH_SHORT).show();
 				finish();
 			} else if (BleService.BLE_CHARACTERISTIC_READ.equals(action)
 					|| BleService.BLE_CHARACTERISTIC_CHANGED.equals(action)) {
 				byte[] val = extras.getByteArray(BleService.EXTRA_VALUE);
-							
+
 				weight = Utils.byte2Int(val);
-				
+
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
@@ -105,7 +103,7 @@ public class WeightActivity extends Activity {
 						txtWgt.setText(String.valueOf(weight));
 					}
 				});
-				
+
 			} else if (BleService.BLE_CHARACTERISTIC_NOTIFICATION
 					.equals(action)) {
 				Toast.makeText(WeightActivity.this,
@@ -113,9 +111,9 @@ public class WeightActivity extends Activity {
 						.show();
 				mNotifyStarted = extras.getBoolean(BleService.EXTRA_VALUE);
 				if (mNotifyStarted) {
-					
+
 				} else {
-					
+
 				}
 			} else if (BleService.BLE_CHARACTERISTIC_INDICATION.equals(action)) {
 				Toast.makeText(WeightActivity.this,
@@ -124,69 +122,69 @@ public class WeightActivity extends Activity {
 				Toast.makeText(WeightActivity.this, "Write success!",
 						Toast.LENGTH_SHORT).show();
 			} else if (BleService.BLE_GATT_CONNECTED.equals(action)) {
-				
-				Toast.makeText(WeightActivity.this,
-						"Connect ok!" + extras.getString(BleService.EXTRA_ADDR), Toast.LENGTH_SHORT).show();
+
+				Toast.makeText(
+						WeightActivity.this,
+						"Connect ok!" + extras.getString(BleService.EXTRA_ADDR),
+						Toast.LENGTH_SHORT).show();
 			} else if (BleService.BLE_GATT_DISCONNECTED.equals(action)) {
-				
+
 			} else if (BleService.BLE_SERVICE_DISCOVERED.equals(action)) {
-				Toast.makeText(WeightActivity.this,
-						"service discovery!" + extras.getString(BleService.EXTRA_ADDR), Toast.LENGTH_SHORT).show();
+				Toast.makeText(
+						WeightActivity.this,
+						"service discovery!"
+								+ extras.getString(BleService.EXTRA_ADDR),
+						Toast.LENGTH_SHORT).show();
 			}
-			
+
 		}
 	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_weight);
-		txtWgt = (TextView)findViewById(R.id.txtWgt);
-		listData = (ListView) findViewById(R.id.list); 
-		
-		adapter = new MyAdapter(this); 
-		listData.setAdapter(adapter); 
-		
-		
-		final Button btnSave = (Button)findViewById(R.id.btn_save);
-			
-		btnSave.setOnClickListener(new OnClickListener() { 
-             @Override 
-             public void onClick(View arg0) { 
-                 // TODO Auto-generated method stub 
-            	 String kgs = txtWgt.getText().toString();
-            	 WeightData data = new WeightData(index++,kgs);
-            	 
-                 adapter.arr.add(data); 
-                 adapter.notifyDataSetChanged(); 
-             } 
-         }); 
+		txtWgt = (TextView) findViewById(R.id.txtWgt);
+		listData = (ListView) findViewById(R.id.list);
 
-		
+		adapter = new MyAdapter(this);
+		listData.setAdapter(adapter);
+
+		final Button btnSave = (Button) findViewById(R.id.btn_save);
+
+		btnSave.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				String kgs = txtWgt.getText().toString();
+				WeightData data = new WeightData(index++, kgs);
+
+				adapter.arr.add(data);
+				adapter.notifyDataSetChanged();
+			}
+		});
 
 		mDeviceAddress = getIntent().getStringExtra("address");
-		String service = getIntent().getStringExtra("service");
-		String characteristic = getIntent().getStringExtra("characteristic");
+		mService = getIntent().getStringExtra("service");
+		mCharacteristics = getIntent().getStringExtra("characteristic");
 		BleApplication app = (BleApplication) getApplication();
 		mBle = app.getIBle();
 		mCharacteristic = mBle.getService(mDeviceAddress,
-				UUID.fromString(service)).getCharacteristic(
-				UUID.fromString(characteristic));
-		if(mCharacteristic == null)
-		{
+				UUID.fromString(mService)).getCharacteristic(
+				UUID.fromString(mCharacteristics));
+		if (mCharacteristic == null) {
 			return;
 		}
-		new Timer().schedule(new TimerTask()
-		{
-			public void run()
-			{
-				
+		new Timer().schedule(new TimerTask() {
+			public void run() {
+
 				mBle.requestReadCharacteristic(mDeviceAddress, mCharacteristic);
 			}
-		},0,100);
-		
-		//mNotifyStarted = true;
-		//mBle.requestCharacteristicNotification(mDeviceAddress,
-		//		mCharacteristic);
+		}, 0, 100);
+
+		// mNotifyStarted = true;
+		// mBle.requestCharacteristicNotification(mDeviceAddress,
+		// mCharacteristic);
 
 	}
 
@@ -204,19 +202,20 @@ public class WeightActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			if (mNotifyStarted) {
-				mBle.requestStopNotification(mDeviceAddress,
-						mCharacteristic);
-			} else {
-				mBle.requestCharacteristicNotification(mDeviceAddress,
-						mCharacteristic);
-			}
+		if (id == R.id.device_settings) {
+	
+			Intent intent = new Intent(WeightActivity.this, CalibActivity.class); 
+			
+			intent.putExtra("address", mDeviceAddress);
+			intent.putExtra("service", mService);
+			intent.putExtra("characteristic", mCharacteristics);
+
+			startActivity(intent);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -224,55 +223,67 @@ public class WeightActivity extends Activity {
 	}
 
 	@Override
-	public void onPanelClosed(int featureId, Menu menu) {
-		super.onPanelClosed(featureId, menu);
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.e(TAG, "onStop");
 		unregisterReceiver(mBleReceiver);
 	}
+
+	@Override
+	public void onPanelClosed(int featureId, Menu menu) {
+		super.onPanelClosed(featureId, menu);
+		Log.e(TAG, "onPanelClosed");
 	
-	private class MyAdapter extends BaseAdapter { 
-	    
-        private LayoutInflater inflater; 
-        public ArrayList<WeightData> arr; 
-        public MyAdapter(Context context) { 
-            super(); 
-            inflater = LayoutInflater.from(context); 
-            arr = new ArrayList<WeightData>(); 
-            for(int i=0;i<3;i++){    //listview��ʼ��3������ 
-                //arr.add(""); 
-            } 
-        } 
-        @Override 
-        public int getCount() { 
-            // TODO Auto-generated method stub 
-            return arr.size(); 
-        } 
-        @Override 
-        public Object getItem(int arg0) { 
-            // TODO Auto-generated method stub 
-            return arg0; 
-        } 
-        @Override 
-        public long getItemId(int arg0) { 
-            // TODO Auto-generated method stub 
-            return arg0; 
-        } 
-        @Override 
-        public View getView(final int position, View view, ViewGroup arg2) { 
-            // TODO Auto-generated method stub 
-            if(view == null){ 
-                view = inflater.inflate(R.layout.listview_item, null); 
-            } 
-            final TextView edit = (TextView) view.findViewById(R.id.index); 
-            edit.setText(arr.get(position).sid);    //���ع�adapter��ʱ��������ݴ��� 
-            final TextView time = (TextView) view.findViewById(R.id.time); 
-            time.setText(arr.get(position).stime);    //���ع�adapter��ʱ��������ݴ��� 
-            final TextView kg = (TextView) view.findViewById(R.id.kg); 
-            kg.setText(arr.get(position).skg);    //���ع�adapter��ʱ��������ݴ��� 
-            
-            return view; 
-        } 
-    } 
+	}
 
+	private class MyAdapter extends BaseAdapter {
 
+		private LayoutInflater inflater;
+		public ArrayList<WeightData> arr;
+
+		public MyAdapter(Context context) {
+			super();
+			inflater = LayoutInflater.from(context);
+			arr = new ArrayList<WeightData>();
+			for (int i = 0; i < 3; i++) {
+				// arr.add("");
+			}
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return arr.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return arg0;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return arg0;
+		}
+
+		@Override
+		public View getView(final int position, View view, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+			if (view == null) {
+				view = inflater.inflate(R.layout.listview_item, null);
+			}
+			final TextView edit = (TextView) view.findViewById(R.id.index);
+			edit.setText(arr.get(position).sid);
+			final TextView time = (TextView) view.findViewById(R.id.time);
+			time.setText(arr.get(position).stime);
+			final TextView kg = (TextView) view.findViewById(R.id.kg);
+			kg.setText(arr.get(position).skg);
+
+			return view;
+		}
+	}
 
 }
